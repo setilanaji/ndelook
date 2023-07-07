7d6af47e4dfe859e0c42be24cdca832786ce146b
yudha setyaji <setyajiyudha299@gmail.com>
2023-07-07 21:21:41 +0700

    Initial Commit

---
+++ ndelook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ndelook
+//
+//  Created by Yudha S on 07/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
